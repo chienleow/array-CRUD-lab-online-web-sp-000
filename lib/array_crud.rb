dfa9064ddb6556ee2cac5af0b3bd7d5e3e9665bd
C. Leow <chienleow1@gmail.com>
2020-02-28 21:41:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  favorite_food = ["sushi","ice cream","fried chicken","soup"]
+  favorite_food.push("ramen")
 end
 
 def add_element_to_start_of_array(array, element)
