9909817959d76dc2054b6a3238f8672540b1d3f0
C. Leow <chienleow1@gmail.com>
2020-02-28 21:48:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
   array.push("arrays!")
-  puts favorite_food.inspect
+  puts array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
